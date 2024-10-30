0789878b0a67473a6bf513d27323c38972210e7e
varunnlv <147477534+varunnlv@users.noreply.github.com>
2024-10-30 14:29:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import Description from "./components/Description";
 import Home from "./components/Home";
 import Sorting from "./components/Sorting";
 import BubbleSort from "./components/BubbleSort";
+import CocktailSort from "./components/CocktailSort";
+import CombSort from "./components/CombSort";
+import GnomeSort from "./components/GnomeSort";
+import QuickSort from "./components/QuickSort";
+import InsrtionSort from "./components/InsrtionSort";
+import SelectionSort from "./components/SelectionSort";
+import Datastructures from "./components/datastructures";
+import Stack from "./components/Stack";
+import Searching from "./components/Searching";
+
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(true); // Modal is open by default
@@ -32,9 +42,18 @@ function App() {
         {/* Define the routes */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/pathfinder" element={<PathfindingVisualizer />} />
+          <Route path="/pathfinder" element={<PathfindingVisualizer />} />   
+          <Route path="/searching" element={<Searching />} />
           <Route path="/sorting" element={<Sorting />} />
           <Route path="/bubblesort" element={<BubbleSort />} />
+          <Route path="/cocktailsort" element={<CocktailSort />} />
+          <Route path="/combsort" element={<CombSort />} />
+          <Route path="/gnomesort" element={<GnomeSort />} />
+          <Route path="/quicksort" element={<QuickSort />} />
+          <Route path="/insertionsort" element={<InsrtionSort />} />
+          <Route path="/selectionsort" element={<SelectionSort />} />
+          <Route path="/datastructures" element={<Datastructures/>}/>
+          <Route path="/stack" element={<Stack/>}/>
         </Routes>
       </div>
     </Router>
